c1ad3cf306c3ecc226965ff60f6ac7dc4ad893f0
samechaaa <kana.sugimoto@glpgs.com>
2025-05-27 19:24:03 +0900

    タスク永続化用のモデルクラスを追加

---
+++ Training/Sources/Data/Sources/Data/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  Training
+//
+//  Created by kana.sugimoto on 2025/05/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
